c8545c22ff01011c8cf99ddb9b32acbecb721b00
Julio <julioraulcordal@gmail.com>
2025-05-21 12:39:46 +0200

    change order url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('login/',authentication_views.LoginView.as_view(template_name ='users/login.html'), name='login'), #class based view (that is why .asview())
     path('logout/',user_views.logout_view, name='logout_view'),
     path('cv_form/',user_views.cv_form, name='cv_form'),
-    path('pdf/<int:id>',user_views.cv_pdf, name='cv_pdf'),
+    path('<int:id>/pdf/',user_views.cv_pdf, name='cv_pdf'),
     path('list/',user_views.list, name='list'),
-    path('cv/<int:id>',user_views.cv, name = 'cv'),
+    path('<int:id>/cv/',user_views.cv, name = 'cv'),
     path('redirect-to-cv/', user_views.redirect_to_cv, name='redirect_to_cv'),
     path('mlmanager/', user_views.magic_link_manager, name='magic_link_manager'),
     path('consent/', user_views.consent_form, name='consent_form'),
