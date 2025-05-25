fed1b6ecf406b36a6508ceb4dced35b6834469d4
Julio <julioraulcordal@gmail.com>
2025-05-25 17:04:12 +0200

    quitado error

--- main_page/views.py
+++ main_page/views.py
@@ -18,16 +18,12 @@ def main(request):
             {message}
             """
 
-        try:
-            send_mail(
+        send_mail(
                 subject="Received contact form submission",
                 message=full_message,
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[settings.NOTIFY_EMAIL],
             )
-        except Exception as e:
-            return HttpResponse("There was an error sending your message. Please try again later", status=500)
-
         return redirect('success')
 
     return render(request, 'main_page/main_page.html')
