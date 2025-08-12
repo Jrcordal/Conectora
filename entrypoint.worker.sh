dd0bd46f316959cd759b86aebae40c6a4e965636
Julio <julioraulcordal@gmail.com>
2025-08-12 14:59:35 +0200

    s

--- entrypoint.worker.sh
+++ entrypoint.worker.sh
@@ -1,5 +1,5 @@
-# entrypoint.worker.sh
 #!/usr/bin/env bash
 set -e
+
+# Lanza el worker de Celery usando la app registrada en __init__.py
 exec celery -A main_page worker -l info
-#                ^^^^^^^^^  cambia por el módulo donde esté tu celery.py
