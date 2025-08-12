2da27fb8b7e8e313d4211c2dcff43bb92f815d3d
Julio <julioraulcordal@gmail.com>
2025-08-12 14:54:03 +0200

    d

--- main_page/celery.py
+++ main_page/celery.py
@@ -1,6 +1,6 @@
 import os
 
-from main_page.celery import Celery
+from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_page.settings')
