91a9feecd04febf35022a0196666a8733da1088c
Julio <julioraulcordal@gmail.com>
2025-08-12 14:57:58 +0200

    d

--- entrypoint.worker.sh
+++ entrypoint.worker.sh
@@ -1,5 +1,5 @@
 # entrypoint.worker.sh
 #!/usr/bin/env bash
 set -e
-exec celery -A main_page.celery worker -l info
+exec celery -A main_page worker -l info
 #                ^^^^^^^^^  cambia por el módulo donde esté tu celery.py
