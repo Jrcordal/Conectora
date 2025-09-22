3ad35bd4ce273ab702ce870cbd3ce5ae8b37ee5f
Julio <julioraulcordal@gmail.com>
2025-09-22 10:26:11 +0200

    d

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -14,7 +14,7 @@ class ClientProfile(models.Model):
     linkedin = models.URLField(blank=True, null=True)
     website = models.URLField(blank=True, null=True)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, blank=True, null=True)
-    role_in_company = models.CharField(max_length=20, blanck = True)
+    role_in_company = models.CharField(max_length=20, blank = True)
     
     def __str__(self):
         return self.user.username
