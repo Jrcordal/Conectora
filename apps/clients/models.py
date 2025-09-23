e5f5b117b9bfddb6a45962972ff6d4ae1ef746b4
Julio <julioraulcordal@gmail.com>
2025-09-23 11:26:18 +0200

    a

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -15,8 +15,7 @@ class ClientProfile(models.Model):
     website = models.URLField(blank=True, null=True)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, blank=True, null=True)
     role_in_company = models.CharField(max_length=50, blank = True)
-    completed_projects = models.IntegerField(blank=True, default=0)
-    active_projects = models.IntegerField(blank=True, default=0)
+    number_of_projects = models.IntegerField(blank=True, default=0)
     total_candidates_matched = models.IntegerField(blank=True, default=0)
     search_limit = models.PositiveIntegerField(blank=True, default=3)
     
@@ -24,12 +23,32 @@ class ClientProfile(models.Model):
         return self.user.username
 
 
+
+class IntakeStage(models.TextChoices):
+    INTAKE_REQUIREMENTS    = "intake_requirements",    "Requirements received"
+    PROPOSAL_DRAFT   = "proposal_draft",  "AI-made proposal draft"
+    TECH_STACK_DRAFT      = "tech_stack_draft",     "Created a tech stack draft"
+    POTENTIAL_CANDIDATES     = "potential_candidates",    "Searched for potential candidates"
+    SELECTED_CANDIDATES  = "selected_candidates", "Selected final candidates"
+
+
+
+class ProjectStatus(models.TextChoices):
+    AI_INTAKE          = "ai_intake", "AI Intake"
+    PRESALE            = "presale", "Presale"
+    CONTRACT_SIGNED    = "contract_signed", "Contract signed"
+    CANCELLED_CONTRACT = "cancelled_contract", "Cancelled contract"
+    COMPLETED          = "completed", "Completed"
+
+
 class Project(models.Model):
     client = models.ForeignKey(ClientProfile, on_delete=models.CASCADE, related_name='clientproject')
-    initial_ask = models.JSONField(blank=True)
-    proposal_draft = models.JSONField(blank=True)
-    tech_stack_draft = models.JSONField(blank=True)
-    potential_candidates = models.JSONField(blank=True)
-    selected_candidates = models.JSONField(blank=True)
+    intake_requirements = models.JSONField(blank=True, null=True, default=None)
+    proposal_draft = models.JSONField(blank=True,  null=True, default=None)
+    tech_stack_draft = models.JSONField(blank=True,  null=True, default=None)
+    potential_candidates = models.JSONField(blank=True,  null=True, default=None)
+    selected_candidates = models.JSONField(blank=True,  null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status_intake = models.CharField(max_length=20, choices=IntakeStage.choices, default=IntakeStage.INTAKE_REQUIREMENTS)
+    status_project = models.CharField(max_length=30, choices=ProjectStatus.choices, default=ProjectStatus.AI_INTAKE)
